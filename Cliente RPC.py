42d6cacaa7a16a467dcaeda2e3d8397c6c361897
Júlio Ferreira <86128073+julioferreirah@users.noreply.github.com>
2023-06-15 19:55:57 -0300

    Tentando fazer o RPC funcionar

--- Cliente RPC.py
+++ Cliente RPC.py
@@ -7,48 +7,55 @@ IP = input('- Digite o IP do Servidor: ')
 PORTA = int(input('- Digite a PORTA: '))
 
 cont = t.time()
-servidor = xmlrpc.client.ServerProxy("http://{0}:{1}/".format(IP, PORTA))
-print("Tempo de conexão: %d" %(t.time() - cont))
+with xmlrpc.client.ServerProxy("http://localhost:8000/") as servidor:
+    print("Tempo de conexão: %d" %(t.time() - cont))
 
-tempovoid = 0
-tempoexp = 0
-temposum = 0
-tempoupper = 0
-tempocomplex = 0
+    tempovoid = 0
+    tempoexp = 0
+    temposum = 0
+    tempoupper = 0
+    tempocomplex = 0
 
-for i in range(5):
-    print("Rodada de requisições: %i" %(i))
-    cont = t.time()
+    for i in range(5):
+        print("Rodada de requisições: %i" %(i))
+        cont = t.time()
 
-    #chamada sem valor
-    c =  t.time()
-    servidor.void()
-    tempovoid += t.time() - c
+        #chamada sem valor
+        c =  t.time()
+        print(servidor.void())
+        tempovoid += t.time() - c
 
-    #chamada de long
-    c =  t.time()
-    servidor.exp(2.5+i)
-    tempoexp += t.time() - c
+        #chamada de long
+        c =  t.time()
+        print(servidor.exp(2.5+i))
+        tempoexp += t.time() - c
 
-    #chamada de lista de long
-    a = [8]
-    for j in range(8):
-        a[j] = float(input("Digite um long:"))
-    c =  t.time()
-    servidor.sum(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7])
+        #chamada de lista de long
+        a = []
+        for j in range(8):
+            a.append((2.5 + i + j)/2)
+        c =  t.time()
+        print(servidor.sum(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]))
+        temposum += t.time() - c
 
-    #chamada de string
-    c =  t.time()
-    servidor.upper(str(input("Digite uma cadeia de 32 caracteres:")))
+        #chamada de string
+        c =  t.time()
+        print(servidor.upper("string de 32 caracteres numero %i" %(i)))
+        tempoupper += t.time() - c
 
-    #chamada de tipo complexo
-    c =  t.time()
-    servidor.complex()
+        #chamada de tipo complexo
+        com = Classe.Class()
+        c =  t.time()
+        servidor.complex(com)
+        tempocomplex += t.time() - c
+        print(com.string, com.int)
 
-    print("Tempo total rodada %i: %d" %(i,t.time() - cont))
+        print("Tempo total rodada %i: %.5f" %(i,t.time() - cont))
+        print("===========================")
 
-print("Tempo médio chamada sem valor", tempovoid/5)
-print("Tempo médio chamada com long", tempoexp/5)
-print("Tempo médio chamada com lista", temposum/5)
-print("Tempo médio chamada com string", tempoupper/5)
-print("Tempo médio chamada com tipo complexo", tempocomplex/5)
+    print("Tempo médio chamada sem valor", tempovoid/5)
+    print("Tempo médio chamada com long", tempoexp/5)
+    print("Tempo médio chamada com lista", temposum/5)
+    print("Tempo médio chamada com string", tempoupper/5)
+    print("Tempo médio chamada com tipo complexo", tempocomplex/5)
+
